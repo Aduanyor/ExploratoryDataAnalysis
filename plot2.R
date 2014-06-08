fc506e03bbd43a18406d05d0e1c79a87525b86aa
Aduanyor <Aduanyor@users.noreply.github.com>
2014-06-08 13:17:26 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,5 +1,5 @@
-## Getting full dataset
-data <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+## Getting dataset
+data <- read.csv("./household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 data$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
 
 ## Subsetting the data
